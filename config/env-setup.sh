b958620c987a41e945a3bc6c7ea4dfda4d707ca3
al <alvminvm@qq.com>
2015-03-05 23:14:31 +0800

    swap caps local and ctrl_r

--- config/env-setup.sh
+++ config/env-setup.sh
@@ -4,3 +4,7 @@ echo "更新软件库"
 sudo apt-get upgrade
 echo "安装32位支持库"
 sudo apt-get install ia32-libs lib32ncurses5-dev
+
+# 需要向rc.local添加的内容，在启动的时候运行
+# echo 'linux' | sudo -S sslocal -c /etc/shadowsocks.json -d start
+# xmodmap ~/.xmodmap 2>/dev/null
