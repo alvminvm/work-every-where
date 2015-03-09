5915a65fdc4d6fc2081f7f3ce6c3009152672f4f
JeremyHe <jeremyhe.cn@gmail.com>
2015-03-09 20:00:14 +0800

    修改xmodmap为Xmodmap；config目录中sh文件加入环境配置文件

--- config/env-setup.sh
+++ config/env-setup.sh
@@ -1,9 +1,15 @@
 #!bin/sh
+
+# 添加环境变量
+. env-settings.sh
+
 # 更新软件库并安装32位的支持
 echo "更新软件库"
-sudo apt-get upgrade
+echo $PWD | sudo -S apt-get upgrade
 echo "安装32位支持库"
-sudo apt-get install ia32-libs lib32ncurses5-dev
+echo $PWD | sudo -S apt-get install ia32-libs lib32ncurses5-dev
+echo "安装下载工具MultiGet"
+echo $PWD | sudo -S apt-get install multiget 
 
 # 需要向rc.local添加的内容，在启动的时候运行
 # echo 'linux' | sudo -S sslocal -c /etc/shadowsocks.json -d start
