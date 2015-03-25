544ad1994b8e8835ff8a009a8a03b58899213302
JeremyHe <JeremyHe.cn@gmail.com>
2015-03-25 22:51:22 +0800

    vim settings

--- .emacs.d/el-get/.status.el
+++ .emacs.d/el-get/.status.el
@@ -13,7 +13,7 @@
  (el-get status "installed" recipe
 	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
 		("el-get.*\\.el$" "methods/")
-		:load "el-get.el" :post-init
+		:features el-get :post-init
 		(when
 		    (memq 'el-get
 			  (bound-and-true-p package-activated-list))
