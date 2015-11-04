6ffb95d0150611892bb3371d0b7dd87b435f5710
JeremyHe <jeremyhe.cn@gmail.com>
2015-11-05 00:03:39 +0800

    zsh

--- .setupMyUbuntu/installGit.sh
+++ .setupMyUbuntu/installGit.sh
@@ -2,7 +2,7 @@
 . env.sh
 
 echo "install git"
-echo $password | sudo -S apt-get install git
+echo $PWD | sudo -S apt-get -y install git
 
 echo "config git"
 git config --global user.email "jeremyhe.cn@gmail.com"
