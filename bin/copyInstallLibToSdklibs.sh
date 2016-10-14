4de51ef1f7f52d37795cc37f6c283c18d1e62e0c
al <alvminvm@qq.com>
2016-10-14 18:14:03 +0800

    2016年10月14日 星期五 18时14分03秒 CST

--- bin/copyInstallLibToSdklibs.sh
+++ bin/copyInstallLibToSdklibs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-rm -R $MMR_DEV_ROOT/sdk/lib/android
-cp -R $MMR_DEV_ROOT/core/install/lib/android $MMR_DEV_ROOT/sdk/lib/android
-rm -R $MMR_DEV_ROOT/sdk/lib/linux
-cp -R $MMR_DEV_ROOT/core/install/lib/linux $MMR_DEV_ROOT/sdk/lib/linux
+target_android=$MMR_DEV_ROOT/sdk/lib/android
+if [ -d $target_android ]; then
+	rm -R $target_android
+fi
+
+src_android=$MMR_DEV_ROOT/core/install/lib/android
+if [ -d $src_android ]; then
+	cp -R $src_android $target_android
+	echo "copy android lib"
+fi
+
+target_linux=$MMR_DEV_ROOT/sdk/lib/linux
+if [ -d $target_linux ]; then
+	rm -R $target_linux 
+fi
+
+src_linux=$MMR_DEV_ROOT/core/install/lib/linux
+if [ -d $src_linux ];then
+	cp -R $src_linux $target_linux
+	echo "copy linux lib"
+fi
