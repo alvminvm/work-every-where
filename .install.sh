600f04705ece600d51524cc943148ee458bd2671
JeremyHe <jeremyhe.cn@gmail.com>
2016-11-19 10:57:09 +0800

    调整install.sh

--- .install.sh
+++ .install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 copy_git(){
-	echo "复制 work-every-where 至根目录"
-	cp -R ./ ~/
+	echo ">>> copy work-every-where to $HOME"
+	sudo cp -R ./ ~/
 }
 
 install_zsh(){
 	if [ -f ~/.zshrc ];then
-		echo "安装 zsh"
-		sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+		echo ">>> install zsh"
+		sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	fi
-	sudo chmod 777 ~/.zshrc
+	sudo chmod 777 ~/.zsh*
+	sudo chmod 777 ~/.zcomp*
 }
 
 install_sh_user(){
 	has_sh_user=`cat ~/.zshrc | grep sh_user`
 	if [ ! "$has_sh_user" ]; then
-		echo "配置 sh_user"
+		echo ">>> config sh_user"
 		echo "" >> ~/.zshrc
 		echo "# source .sh_user" >> ~/.zshrc
 		echo "test -e ~/.sh_user && source ~/.sh_user" >> ~/.zshrc
@@ -26,16 +27,35 @@ install_sh_user(){
 install_shell_integration(){
 	has_iterm2=`cat ~/.zshrc | grep iterm2`
 	if [ ! "$has_iterm2" ]; then
-		echo "安装 shell integration"
+		echo ">>> install shell integration"
 		echo "" >> ~/.zshrc
 		echo "# iterm2" >> ~/.zshrc
 		curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
 	fi
 }
 
-sudo echo "开始配置"
+config_git(){
+	echo ">>> config git"
+	git config --global user.name $username
+	git config --global user.email $email
+	echo "Current git config:"
+	git config --global user.name
+	git config --global user.email
+}
+
+read -p "Enter ur name(JeremyHe): " username
+if [ ! "$username" ]; then
+	username="JeremyHe"
+fi
+read -p "Enter ur email(jeremyhe.cn@gmail.com): " email
+if [ ! "$email" ]; then
+	email="jeremyhe.cn@gmail.com"
+fi
+sudo echo ">> start install"
 copy_git;
 install_sh_user;
 install_shell_integration;
+config_git;
+echo ">> install finish"
 
 
