87e16094f77387ee4fc2ba851d38dc8c764f514c
al <alvminvm@qq.com>
2025-01-01 22:49:08 +0800

    improve(flutter): add pub-cache/bin

--- path.sh
+++ path.sh
@@ -4,6 +4,7 @@ export FLUTTER_PATH=$FLUTTER_HOME/bin
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 export PATH=$FLUTTER_PATH:$PATH
+export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 # ruby
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
